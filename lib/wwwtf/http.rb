2d0aa6c3f2973e095266e2a0884b1b20a14ce82f
AbhishekKr <abhikumar163@gmail.com>
2013-02-06 18:26:53 +0530

    curl headers corrected for quotes, 400/500 responses to be handled

--- lib/wwwtf/http.rb
+++ lib/wwwtf/http.rb
@@ -9,8 +9,9 @@ module Wwwtf
     @curl_headers = 'User Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, Linuxke Gecko) Chrome/22.0.1229.52 Safari/537.4'
 
     def self.headers(url)
-      hdrs = %x{curl -IkLs -H #{curl_headers} #{url}}
+      hdrs = %x{curl -IkLs -H '#{curl_headers}' #{url}}
       headr = {}
+      return headr if hdrs.match(/HTTP\/1\.[01]\s*[45][0-9][0-9]\s*\w/)
       hdr = hdrs.split("\r\n")
       hdr = hdr.collect{|h| {h.split(':')[0].strip => h.split(':')[1..-1].join.strip} }
       hdr.each{|h| headr = headr.merge h}
@@ -18,14 +19,14 @@ module Wwwtf
     end
 
     def self.body(url)
-      %x{curl -kLs -H #{curl_headers} #{url}}
+      %x{curl -kLs -H '#{curl_headers}' #{url}}
     end
 
     def self.page_dependency(url)
-      body       = body url
-      xtmp_nodes = XMLMotor.splitter body
-      xtmp_tags  = XMLMotor.indexify xtmp_nodes
-      tag_src    = XMLMotor.xmlattrib 'src', xtmp_nodes, xtmp_tags
+      page_content = body url
+      xtmp_nodes   = XMLMotor.splitter page_content
+      xtmp_tags    = XMLMotor.indexify xtmp_nodes
+      tag_src      = XMLMotor.xmlattrib 'src', xtmp_nodes, xtmp_tags
       src_in_page = tag_src.uniq.collect{|src|
                       fix_urls url, src
                     }
